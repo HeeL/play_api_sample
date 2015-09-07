f88fff57bcd5aa4cdb1b6dfe41dbc495fccee3ef
Sergii Paryzhskyi <parizhskiy@gmail.com>
2015-09-07 22:37:59 +0200

    books controller with index action to list books

---
+++ app/controllers/Books.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import models.Book
+
+class Books extends Controller {
+
+  def index = Action {
+    Ok(Json.toJson(Book.books))
+  }
+
+}
